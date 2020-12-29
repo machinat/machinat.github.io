f3b5c32bef5589e40748789811be6c79ceb76315
Patrick Liu <zeldalink0515@gmail.com>
2020-12-29 11:20:34 +0800

    Done content parts

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,30 +1,34 @@
 module.exports = {
-  title: 'My Site',
-  tagline: 'The tagline of my site',
-  url: 'https://your-docusaurus-test-site.com',
+  title: 'Machinat',
+  tagline: 'Build Next-Gen Chat App on ALL Conversational Platforms',
+  url: 'https://machinat.com',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'machinat',
+  projectName: 'machinat',
   themeConfig: {
     navbar: {
-      title: 'My Site',
       logo: {
         alt: 'My Site Logo',
-        src: 'img/logo.svg',
+        src: 'img/nav-logo.svg',
+        srcDark: 'img/nav-logo-dark.svg',
       },
       items: [
         {
           to: 'docs/',
           activeBasePath: 'docs',
           label: 'Docs',
-          position: 'left',
+          position: 'right',
         },
-        {to: 'blog', label: 'Blog', position: 'left'},
         {
-          href: 'https://github.com/facebook/docusaurus',
+          to: 'blog',
+          label: 'Blog',
+          position: 'right',
+        },
+        {
+          href: 'https://github.com/machinat/machinat',
           label: 'GitHub',
           position: 'right',
         },
@@ -77,7 +81,7 @@ module.exports = {
           ],
         },
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      copyright: `Copyright © ${new Date().getFullYear()} Machinat LLC`,
     },
   },
   presets: [
@@ -88,13 +92,13 @@ module.exports = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/',
+            'https://github.com/machinat/docusaurus/edit/master/website/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/blog/',
+            'https://github.com/machinat/docusaurus/edit/master/website/blog/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,91 +4,148 @@ import Layout from '@theme/Layout';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import useBaseUrl from '@docusaurus/useBaseUrl';
+import CodeBlock from '@theme/CodeBlock';
+import SwitchDarkImage from '../components/SwitchDarkImage';
+import NeonWords from '../components/NeonWords';
 import styles from './styles.module.css';
 
 const features = [
   {
-    title: 'Easy to Use',
-    imageUrl: 'img/undraw_docusaurus_mountain.svg',
+    title: 'One App for All Platforms',
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        Today businesses are doing marketing and customer services through many
+        conversational platforms, from <i>Email</i>, <i>Instant Messaging</i> to
+        <i>Social Medias</i> to <i>Voice Assistance</i>.
+        <br/>
+        Develop a chat app with Machinat once, then you can ship services over
+        all these channels and reach more users.
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
-    imageUrl: 'img/undraw_docusaurus_tree.svg',
+    title: 'Hybrid of Chat and Web UI',
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        Conversational UI is easy to access, but not as functional as graphical
+        UI. With Machinat, you can have both advantages by opening an embeded
+        webview in the chatroom.
+        <br/>
+        The Hybrid App can improve the expereience of complex actions and
+        provide more enriched features.
       </>
     ),
   },
   {
-    title: 'Powered by React',
-    imageUrl: 'img/undraw_docusaurus_react.svg',
+    title: 'Declarative API for Omni-Channel Experiences',
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        Machinat use JSX API like React to contstruct chat UI in declarative
+        views. The view is composed by flexible components, which allows you
+        modify the expression while rendering.
+        <br/>
+        This helps to build aligned and integrated experiences across diverse
+        channels, and meanwhile make optimization for every platform.
+      </>
+    ),
+  },
+  {
+    title: 'Natural Marketing with Social Network',
+    description: (
+      <>
+        Machinat app built on <i>Instant Messaging</i> and <i>Social Media</i>
+        has native potential to go viral.
+        <br/>
+        Adding features to allow your app to be used in group chat or
+        community. Users would be glad to share proactively, and the app can be
+        marketed automatically by itself.
+      </>
+    ),
+  },
+  {
+    title: 'Open-Source, Pluginable and Extensible',
+    description: (
+      <>
+        Machinat framework is and will be open-source. No worry about
+        being bound to specific infrastructure provider.
+        <br/>
+        It's also easy to develop plugins, UI components, or even your own
+        event-based platforms.
+        <br/>
+        Enjoy the ecosystem without any restriction!
       </>
     ),
   },
 ];
 
-function Feature({imageUrl, title, description}) {
-  const imgUrl = useBaseUrl(imageUrl);
+function Feature({title, description}) {
   return (
-    <div className={clsx('col col--4', styles.feature)}>
-      {imgUrl && (
-        <div className="text--center">
-          <img className={styles.featureImage} src={imgUrl} alt={title} />
+    <section className={styles.featureContainer}>
+      <div className="container">
+        <div className="row">
+          <div className={clsx('col col--7', styles.zzz)}>
+            <h3 className={styles.featureTitle}>{title}</h3>
+            <p className={styles.featureContent}>{description}</p>
+          </div>
+          <div className={clsx('col col--5', styles.xxx)}>
+          </div>
         </div>
-      )}
-      <h3>{title}</h3>
-      <p>{description}</p>
-    </div>
+      </div>
+    </section>
   );
 }
 
 function Home() {
   const context = useDocusaurusContext();
   const {siteConfig = {}} = context;
+
   return (
     <Layout
       title={`Hello from ${siteConfig.title}`}
       description="Description will go into a meta tag in <head />">
-      <header className={clsx('hero hero--primary', styles.heroBanner)}>
-        <div className="container">
-          <h1 className="hero__title">{siteConfig.title}</h1>
-          <p className="hero__subtitle">{siteConfig.tagline}</p>
-          <div className={styles.buttons}>
+      <header className={clsx('hero hero--primary-lightest', styles.heroBanner)}>
+        <SwitchDarkImage
+          className={styles.heroBannerBackground}
+          defaultSrc="img/hero-background.svg"
+          darkSrc="img/hero-background-dark.svg"
+        />
+
+        <div className={clsx(styles.heroContentContainer, 'container')}>
+          <SwitchDarkImage
+            className={styles.heroLogo}
+            defaultSrc="img/logo.svg"
+            darkSrc="img/logo-dark.svg"
+          />
+
+          <p className={clsx('hero__subtitle', styles.heroSubtitle)}>
+            <div>Build Next-Gen Chat App</div>{' '}
+            <div>on ALL <NeonWords>Conversational Platforms</NeonWords></div>
+          </p>
+          <div className={styles.buttonsContainer}>
             <Link
               className={clsx(
                 'button button--outline button--secondary button--lg',
-                styles.getStarted,
+                styles.button,
               )}
               to={useBaseUrl('docs/')}>
               Get Started
             </Link>
+            <Link
+              className={clsx(
+                'button button--outline button--secondary button--lg',
+                styles.button,
+              )}
+              to="https://github.com/machinat/machinat">
+              View Source
+            </Link>
           </div>
         </div>
       </header>
+
       <main>
-        {features && features.length > 0 && (
-          <section className={styles.features}>
-            <div className="container">
-              <div className="row">
-                {features.map((props, idx) => (
-                  <Feature key={idx} {...props} />
-                ))}
-              </div>
-            </div>
-          </section>
-        )}
+        {features && features.map((props, idx) => (
+          <Feature key={idx} {...props} />
+        ))}
       </main>
     </Layout>
   );
