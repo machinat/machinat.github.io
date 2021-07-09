be010f768b4191bde4577c42873e2a0966bd89ed
Patrick Liu <zeldalink0515@gmail.com>
2021-07-09 15:55:25 +0800

    Deploy website - based on d68a3f480d517af46fb705821cd14c923f6d2a9b

---
+++ assets/js/d2fe0480.f138ff2d.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[31],{102:function(n){n.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"Introducing Machinat","permalink":"/blog/hola"}]}')}}]);
